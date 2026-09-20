4554b4d5517f6cdd5e52b00c42a0c8dba01d1326
Aldiwstech <aldiapriansyah12@gmail.com>
2026-09-20 19:36:27 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ MOCKUP_CANDIDATES = [
     # It must contain the 10 Site Info rows (including Class Site + VIP).
     "wide_clean_infographic_dashboard_ui_mockup_on_a_l.png",
     "Mokup(2).png",
-    "Mokup(1).png", "Mokup.png", "mokup.png", "mokup(1).png",
+    "Mokup(1).png", "Mokupnew.png", "mokup.png", "mokup(1).png",
     "wide_clean_infographic_dashboard_ui_image_in_a_fla.png",
     "Mokup(2).png",
     "Mokup(1).png", "Mokup.png", "mokup.png", "mokup(1).png",
